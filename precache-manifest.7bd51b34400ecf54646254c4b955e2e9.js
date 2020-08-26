c22072e0d1a1ee83b744e9cdd519e13e7d3663eb
vamsi krish <vamsikrishchiguruwada@gmail.com>
2020-08-27 03:37:23 +0530

    Updates

---
+++ precache-manifest.7bd51b34400ecf54646254c4b955e2e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd69b72935169aa47b3b3391c865526",
+    "url": "/samplev1/index.html"
+  },
+  {
+    "revision": "9d5e75f84a86896dc6e4",
+    "url": "/samplev1/static/css/main.44a3c239.chunk.css"
+  },
+  {
+    "revision": "b98c851cfc8fe082f42b",
+    "url": "/samplev1/static/js/2.8b24df94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samplev1/static/js/2.8b24df94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5e75f84a86896dc6e4",
+    "url": "/samplev1/static/js/main.25b8d8f9.chunk.js"
+  },
+  {
+    "revision": "766b16e47daeae4e284d",
+    "url": "/samplev1/static/js/runtime-main.46940b17.js"
+  }
+]);
